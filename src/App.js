239c4a00c6dad8cf5e5869400b24053eb035bc43
Pedro henrick <pedro.henrick@jussi.local>
2019-04-26 13:23:04 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return (
-		<div className="map"></div>
-	);
-}
-
-function initMap(params) {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: -23.584642,
-			lng: -46.675428
-		},
-		zoom: 13
-	});
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
