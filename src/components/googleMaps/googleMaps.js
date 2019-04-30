309ecc564df21881a1f67028e19590524b279b18
unknown <pedro.henrickcs@gmail.com>
2019-04-30 00:10:04 -0300

    marker click

--- src/components/googleMaps/googleMaps.js
+++ src/components/googleMaps/googleMaps.js
@@ -3,6 +3,23 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapGoogle extends Component {
+
+    state = {
+        place: {},
+        marker: {},
+        showInfo: false
+    }
+
+    clickMap = (props, active) => {
+        console.log('maark', props);
+        
+        this.setState({
+            place: props,
+            marker: active,
+            showInfo: true
+        });
+    }
+
     render() {
         console.log('render', this.props);
         return (
@@ -13,11 +30,16 @@ export class MapGoogle extends Component {
                     initialCenter={{ lat: -23.5843807, lng: -46.6784441 }}>
                     
                     <Marker
-                        onClick={() => {
-                            // this.onMarkerClick
-                            console.log('sdfsd');                            
-                        }}
+                        onClick={() => this.clickMap()}
                         name={'Current location'} />
+
+                    <InfoWindow
+                        marker={this.state.marker}
+                        onOpen={this.windowHasOpened}
+                        info={this.showInfo}
+                    >
+
+                    </InfoWindow>
                 </Map>
             </div>
         )
